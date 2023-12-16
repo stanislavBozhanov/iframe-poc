b064049a3322811a1bb429e2316596bfb76d3b81
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2023-12-16 12:19:24 +0200

    Clean project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
 
 function App() {
+  const scriptToRun = `fetch('http://localhost:3000/sensitive', {
+      method: 'POST',
+      body: 'Hello from the iframe!',
+      credentials: "include"
+    });
+    try {
+      console.log("local storage from iframe", localStorage.getItem("thekey"));
+    } catch (e) {
+      console.log("unable to read local storage from iframe");
+    }
+    `;
+
+  const iframeContent = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Iframe Content</title>
+    </head>
+    <body>
+        <h1>Hello from the Iframe!</h1>
+        <p>This iframe has a different origin.</p>
+        <script>
+            ${scriptToRun}
+        </script>
+        <button onclick="sendMessage()">Send Message to Parent</button>
+    </body>
+    </html>
+  `;
+
+  useEffect(() => {
+    // Listen for messages from the iframe
+    window.addEventListener('message', (event) => {
+      console.log('Received message:', event.data);
+    });
+    localStorage.setItem('thekey', 'thevalue');
+    console.log('local storage from self', localStorage.getItem('thekey'));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello World!</p>
+        <p>IFRame</p>
+        <iframe
+          title="my-iframe"
+          srcDoc={iframeContent}
+          style={{ width: '300px', height: '200px' }}
+          sandbox="allow-scripts"
+        />
       </header>
     </div>
   );
